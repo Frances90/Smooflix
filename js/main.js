4c68230243b6a22361caf54128515cdb703d65b1
Shadowlein <francesschmidt@hotmail.de>
2025-08-27 10:22:56 +0200

    LocalStorage Smoothie Daten gespeichert und abgerufen

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@
 /* Smoothie generieren Button ein click Event geben */
 document.getElementById('button_smoothie').addEventListener('click', function() {
     var nameValue = document.getElementById('input_smoothie_name').value;
+    localStorage.setItem('inputWert',nameValue);
     
     document.getElementById("zutaten-list").innerHTML = "";
-    
+
     ladeDaten(nameValue);
 
     document.getElementById('input_smoothie_name').value = '';
@@ -21,13 +22,11 @@ async function ladeDaten(nameSmoothie) {
         console.error("Fehler:", error);
         alert("Fehler beim Laden der Daten.");
     }
-
-    
+  
         console.log(daten.data.name);
         document.getElementById("smoothie_name").innerHTML = daten.data.name;
         document.getElementById("img_smoothie").src = daten.data.image;
         document.getElementById("p_geschmachsrichtung").innerHTML = daten.data.taste;
-
 
         for(var i = 0; i < daten.data.ingredients.length; i++){
             console.log(daten.data.ingredients[i]);
@@ -38,8 +37,43 @@ async function ladeDaten(nameSmoothie) {
             // Das neue Element zur Liste hinzufügen
             document.getElementById('zutaten-list').appendChild(newZutat);
         }
+
+        localStorage.setItem('smoothie', JSON.stringify(daten));
     
 }
+
+function datenAuslesen(){
+
+    /* Inputfeld wieder aus dem Lokalstorage laden */
+    document.getElementById('input_smoothie_name').value = localStorage.getItem('inputWert');
+
+    /* Daten für den Smoothie aus dem Lokalstorage laden */
+    var smoothieobjekt = localStorage.getItem('smoothie');
+    var smoothieobjParse = JSON.parse(localStorage.getItem('smoothie'));
+
+    document.getElementById("smoothie_name").innerHTML = smoothieobjParse.data.name;
+    document.getElementById("img_smoothie").src = smoothieobjParse.data.image;
+    document.getElementById("p_geschmachsrichtung").innerHTML = smoothieobjParse.data.taste;
+
+    for(var i = 0; i < smoothieobjParse.data.ingredients.length; i++){
+            console.log(smoothieobjParse.data.ingredients[i]);
+            var newZutat = document.createElement('li');
+            newZutat.textContent = smoothieobjParse.data.ingredients[i];
+            newZutat.id = "li_zutat";
+                        
+            // Das neue Element zur Liste hinzufügen
+            document.getElementById('zutaten-list').appendChild(newZutat);
+    }
+
+
+    /* Du hast Smooflix bereits programmiert – stark! 
+    Jetzt soll es noch ein bisschen smarter werden: 
+    Beim Neuladen der Seite soll das letzte Suchergebnis automatisch wieder angezeigt werden. 
+    Auch das Suchfeld soll den zuletzt eingegebenen Namen behalten.
+    Speichere hierzu diese Daten im LocalStorage. */
+}
+
+datenAuslesen();
 
 
 
